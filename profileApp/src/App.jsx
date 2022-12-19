4ddca22c76bf2d681f4dd7fa4fc90069561164fd
Fespis <nikita.zhuk00@gmail.com>
2022-12-20 00:56:15 +0300

    feat: add navigation menu

--- profileApp/src/App.jsx
+++ profileApp/src/App.jsx
@@ -1,48 +1,32 @@
 import { Box, Button, Group, Image } from "@mantine/core";
 import mainImage from "./images/main.jpg";
+import Navigation from "./components/nav/Navigation";
+import { useMediaQuery } from "@mantine/hooks";
 
 function App() {
+  const HEIGHT_NAV = 120;
+  const mobile = useMediaQuery(`(max-width: 1060px)`);
+
   return (
     <Box sx={{ color: "#fff" }}>
       <Box sx={{ position: "relative" }}>
-        <Image fit="fill" height={"80vh"} src={mainImage} />
+        <Image
+          fit="fill"
+          height={`calc(100vh - ${HEIGHT_NAV}px)`}
+          src={mainImage}
+        />
         <Box
           sx={{
             position: "absolute",
             bottom: "6%",
             left: "4%",
-            fontSize: "2em",
+            fontSize: `${mobile ? "1.5em" : "2em"}`,
           }}
         >
           Anna Kalakoltsau
         </Box>
       </Box>
-      <Group
-        sx={{
-          backgroundColor: "#11171D",
-          width: "inherit",
-          height: "20vh",
-        }}
-      >
-        <Button variant="outline" color="gray" radius="xl">
-          Start
-        </Button>
-        <Button variant="outline" color="gray" radius="xl">
-          Biography
-        </Button>
-        <Button variant="outline" color="gray" radius="xl">
-          Gallery
-        </Button>
-        <Button variant="outline" color="gray" radius="xl">
-          Video
-        </Button>
-        <Button variant="outline" color="gray" radius="xl">
-          Calendar
-        </Button>
-        <Button variant="outline" color="gray" radius="xl">
-          Contacts
-        </Button>
-      </Group>
+      <Navigation height={HEIGHT_NAV} />
     </Box>
   );
 }
---
+++ profileApp/src/components/nav/Navigation.jsx
@@ -0,0 +1,132 @@
+import {
+  Autocomplete,
+  Burger,
+  Button,
+  Container,
+  createStyles,
+  Divider,
+  Drawer,
+  Group,
+  ScrollArea,
+  Stack,
+} from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
+import { useState } from "react";
+
+const useStyles = createStyles((theme) => ({
+  buttonLink: {
+    padding: "10px 20px",
+    width: 140,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    border: "1px solid #fff",
+    borderRadius: 50,
+    transition: "all ease 0.5s",
+    cursor: "pointer",
+    [theme.fn.smallerThan("1060")]: {
+      display: "none",
+    },
+
+    "&:hover": {
+      backgroundColor: "#fff",
+      color: "#000",
+    },
+  },
+
+  // burgerMobile: {
+  //   display: "none",
+  //   height: 36,
+  //   cursor: "pointer",
+  //   [theme.fn.smallerThan("sm")]: {
+  //     display: "block",
+  //   },
+  //   "& span": {
+  //     margin: 0,
+  //   },
+  // },
+}));
+
+function Navigation(props) {
+  const { classes } = useStyles();
+  const [opened, setOpened] = useState(false);
+  const mobile = useMediaQuery(`(max-width: 1060px)`);
+
+  return (
+    <>
+      <Group
+        position={mobile ? "right" : "center"}
+        spacing={100}
+        p={"lg"}
+        sx={{
+          backgroundColor: "#11171D",
+          width: "inherit",
+          height: `${props.height}px`,
+        }}
+      >
+        {mobile ? (
+          <Burger
+            color="#fff"
+            opened={opened}
+            onClick={() => setOpened((state) => !state)}
+          />
+        ) : (
+          <>
+            <Group spacing="lg" position="left" sx={{ fontSize: "0.7em" }}>
+              <div className={`${classes.buttonLink}`}>Start</div>
+              <div className={`${classes.buttonLink}`}>Biography</div>
+              <div className={`${classes.buttonLink}`}>Gallery</div>
+            </Group>
+            <Group spacing="lg" position="right" sx={{ fontSize: "0.7em" }}>
+              <div className={`${classes.buttonLink}`}>Video</div>
+              <div className={`${classes.buttonLink}`}>Calendar</div>
+              <div className={`${classes.buttonLink}`}>Contacts</div>
+            </Group>
+          </>
+        )}
+      </Group>
+      <Drawer
+        opened={opened}
+        onClose={() => setOpened(false)}
+        position="top"
+        size="full"
+        transition="slide-down"
+        transitionDuration={1000}
+        withCloseButton={false}
+      >
+        <Group
+          p={"lg"}
+          sx={{
+            position: "absolute",
+            right: 1,
+            width: "inherit",
+            height: `${props.height}px`,
+          }}
+        >
+          <Burger
+            color="#000"
+            opened={opened}
+            onClick={() => setOpened((state) => !state)}
+          />
+        </Group>
+        <Stack sx={{ height: "100vh" }} justify="center">
+          <Container>Start</Container>
+          <Container>Biography</Container>
+          <Container>Gallery</Container>
+          <Container>Video</Container>
+          <Container>Calendar</Container>
+          <Container>Contacts</Container>
+        </Stack>
+      </Drawer>
+      <Group
+        sx={{
+          backgroundColor: "#f123",
+          width: "inherit",
+          height: 1200,
+        }}
+      ></Group>
+    </>
+  );
+}
+
+export default Navigation;
