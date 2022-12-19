7f7ce2b000685799d314f4d2b552bcb2ef70513f
Fespis <nikita.zhuk00@gmail.com>
2022-12-19 20:30:28 +0300

    feat: add fonts

--- profileApp/src/App.jsx
+++ profileApp/src/App.jsx
@@ -1,5 +1,50 @@
+import { Box, Button, Group, Image } from "@mantine/core";
+import mainImage from "./images/main.jpg";
+
 function App() {
-  return <div>asd</div>;
+  return (
+    <Box sx={{ color: "#fff" }}>
+      <Box sx={{ position: "relative" }}>
+        <Image fit="fill" height={"80vh"} src={mainImage} />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: "6%",
+            left: "4%",
+            fontSize: "2em",
+          }}
+        >
+          Anna Kalakoltsau
+        </Box>
+      </Box>
+      <Group
+        sx={{
+          backgroundColor: "#11171D",
+          width: "inherit",
+          height: "20vh",
+        }}
+      >
+        <Button variant="outline" color="gray" radius="xl">
+          Start
+        </Button>
+        <Button variant="outline" color="gray" radius="xl">
+          Biography
+        </Button>
+        <Button variant="outline" color="gray" radius="xl">
+          Gallery
+        </Button>
+        <Button variant="outline" color="gray" radius="xl">
+          Video
+        </Button>
+        <Button variant="outline" color="gray" radius="xl">
+          Calendar
+        </Button>
+        <Button variant="outline" color="gray" radius="xl">
+          Contacts
+        </Button>
+      </Group>
+    </Box>
+  );
 }
 
 export default App;
