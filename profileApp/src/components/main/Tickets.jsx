bd87464d609854dd38b22da60a2a03fd98ea98e7
Fespis <nikita.zhuk00@gmail.com>
2023-01-03 02:40:20 +0300

    feat: adaptive for tickets

--- profileApp/src/components/main/Tickets.jsx
+++ profileApp/src/components/main/Tickets.jsx
@@ -1,4 +1,5 @@
-import { createStyles, Group, Stack } from "@mantine/core";
+import { Accordion, Box, createStyles, Group, Stack } from "@mantine/core";
+import { useMediaQuery } from "@mantine/hooks";
 import { useState } from "react";
 
 const useStyles = createStyles((theme) => ({
@@ -9,11 +10,49 @@ const useStyles = createStyles((theme) => ({
     cursor: "pointer",
     textDecoration: "none",
   },
+
+  accordionPanel: {
+    gap: 10,
+    display: "flex",
+    justifyContent: "space-between",
+  },
+
+  rootAccordion: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+  },
+
+  item: {
+    backgroundColor:
+      theme.colorScheme === "dark"
+        ? theme.colors.dark[6]
+        : theme.colors.gray[0],
+    border: "1px solid transparent",
+    position: "relative",
+    zIndex: 0,
+    transition: "transform 150ms ease",
+
+    "&[data-active]": {
+      backgroundColor:
+        theme.colorScheme === "dark" ? theme.colors.dark[7] : theme.white,
+      boxShadow: theme.shadows.md,
+      borderColor:
+        theme.colorScheme === "dark"
+          ? theme.colors.dark[4]
+          : theme.colors.gray[2],
+
+      zIndex: 1,
+    },
+  },
 }));
 
 function Tickets(props) {
   const { mainData } = props;
   const { classes } = useStyles();
+  const mobile = useMediaQuery(`(max-width: 1060px)`);
+
   const [showTicketsInfo, setShowTicketsInfo] = useState(
     Boolean(mainData.ticketsInfo.length)
   );
@@ -43,11 +82,53 @@ function Tickets(props) {
           <Group position="center" mb={60}>
             Tickets
           </Group>
-          <Stack sx={{ fontSize: "0.5em", gap: 60 }}>
+          <Stack sx={{ fontSize: "0.5em", gap: mobile ? 0 : 60 }}>
             {showTicketsInfo ? (
-              mainData.ticketsInfo.map((ticket) => {
-                return (
-                  <Group grow>
+              <Accordion
+                variant="filled"
+                defaultValue="customization"
+                classNames={classes}
+                className={classes.rootAccordion}
+              >
+                {mainData.ticketsInfo.map((tickets, index) => {
+                  return "";
+                })}
+              </Accordion>
+            ) : (
+              <Group position="center">No ticket details</Group>
+            )}
+            {showTicketsInfo ? (
+              mainData.ticketsInfo.map((ticket, index) => {
+                return mobile ? (
+                  <Accordion
+                    variant="filled"
+                    defaultValue="customization"
+                    classNames={classes}
+                    className={classes.rootAccordion}
+                    key={index}
+                  >
+                    <Accordion.Item value="focus-ring">
+                      <Accordion.Control>
+                        {ticket.date}
+                        <br />
+                        {ticket.city}
+                      </Accordion.Control>
+                      <Accordion.Panel>
+                        <Box className={classes.accordionPanel}>
+                          {ticket.place}
+                          <br />
+                          <a
+                            href={ticket.link}
+                            className={classes.buttonTickets}
+                          >
+                            Tickets
+                          </a>
+                        </Box>
+                      </Accordion.Panel>
+                    </Accordion.Item>
+                  </Accordion>
+                ) : (
+                  <Group grow key={index}>
                     <Group>{ticket.date}</Group>
                     <Group position="center">{ticket.city}</Group>
                     <Group position="center">{ticket.place}</Group>
